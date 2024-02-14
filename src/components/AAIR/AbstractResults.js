5f0b8a8bab46df4f23afa9e58ebeb22266246834
Tejas Pethkar <tejas.pethkar@Tejas-Pethkars-MacBook-Pro.local>
2024-02-14 10:35:51 +0000

    calculate percentage added

--- src/components/AAIR/AbstractResults.js
+++ src/components/AAIR/AbstractResults.js
@@ -87,7 +87,7 @@ const AbstractResults = () => {
         borderColor: "#ed64a6",
         data: [],
         fill: false,
-        barThickness: 8,
+        barThickness: 10,
       },
     ],
   });
@@ -104,7 +104,13 @@ const AbstractResults = () => {
       setPercentage(0);
     }
   }, [allAbstractResults.length, numOfExamples]);
-
+  // useEffect(() => {
+  //   // Recalculate percentage whenever there's a relevant change
+  //   if (numOfExamples > 0) {
+  //     const newPercentage = Math.round((allAbstractResults.length / numOfExamples) * 100);
+  //     setPercentage(newPercentage); // Assuming setPercentage updates a state variable
+  //   }
+  // }, [allAbstractResults.length, numOfExamples]); 
   const getAllResultsSafely = useCallback(() => {
     if (!isProcessing && !isStopping) {
       clearTimeout(timerRef.current);
@@ -198,15 +204,15 @@ const AbstractResults = () => {
       // Increment the total count
       resultCount.total++;
     });
-    console.log(resultCount);
     return resultCount;
   };
   useEffect(() => {
     setRowData(allAbstractResults);
     countResults(allAbstractResults);
+    calculatePercentage();
     // Convert the countResults output to chartData format
     const resultsCount = countResults(allAbstractResults);
-    const labels = Object.keys(resultsCount).filter(key => key !== 'total'); // Exclude 'total' from labels
+    const labels = Object.keys(resultsCount).filter((key) => key !== "total"); // Exclude 'total' from labels
     // const labels = Object.keys(resultsCount); // Include 'total' in labels
     const data = labels.map((label) => resultsCount[label]);
 
@@ -214,7 +220,7 @@ const AbstractResults = () => {
       labels: labels,
       datasets: [
         {
-          label: "Results Count : " + resultsCount.total ,
+          label: "Results Count",
           backgroundColor: [
             "#F87171",
             "#FB923C",
@@ -233,7 +239,7 @@ const AbstractResults = () => {
           ],
           data: data,
           fill: false,
-          barThickness: 8,
+          barThickness: 10,
         },
       ],
     });
@@ -362,13 +368,24 @@ const AbstractResults = () => {
               className="text-xs overflow-auto h-30v bg-blueGray-100 p-1"
               dangerouslySetInnerHTML={{ __html: abstractNerMappedText }}
             />
-            <Alert
-              alertClass="bg-emerald-500"
-              alertTitle="Result:"
-              alertMessage={singleAbstractResult.toUpperCase()}
-              showCloseButton={false}
-            />
-            <div className="mt-4">
+            {singleAbstractResult !== "" && (
+              <div className="flex justify-center items-center mt-2">
+                <div className="w-1/12">
+                  {/* Adjust this width as necessary */}
+                  <Alert
+                    alertClass={`${
+                      singleAbstractResult === "include"
+                        ? "bg-emerald-500"
+                        : "bg-orange-500"
+                    }`}
+                    alertTitle="Result:"
+                    alertMessage={singleAbstractResult.toUpperCase()}
+                    showCloseButton={false}
+                  />
+                </div>
+              </div>
+            )}
+            <div>
               <button
                 className={`bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ${
                   currentAbstractId === 0
@@ -397,115 +414,117 @@ const AbstractResults = () => {
           </div>
         </div>
       )}
-      <div className="flex"> 
-      <div className="relative w-9/12 break-words bg-white rounded mb-4 shadow-lg m-2 ml-0">
-        <div className="flex-auto p-4">
-          <div
-            className={`ag-theme-alpine ${
-              rowData.length === 0 && isRefreshing ? "opacity-20" : ""
-            }`}
-            style={{ height: "60vh" }}
-          >
-            <AgGridReact
-              ref={gridRef}
-              rowData={rowData}
-              columnDefs={columnDefs}
-              defaultColDef={defaultColDef}
-              animateRows={true}
-              readOnlyEdit={true}
-              enableCellChangeFlash={true}
-              paginationAutoPageSize={true}
-              pagination={true}
-              suppressClickEdit={true}
-              getRowStyle={getRowStyle}
-            />
-          </div>
-          {isProcessing && (
-            <div className=" mx-auto w-9/12">
-              <ProgressBar
-                taskInProgress="Processing Abstracts ..."
-                percentage={percentage}
+      <div className="flex">
+        <div className="relative w-9/12 break-words bg-white rounded mb-4 shadow-lg m-2 ml-0">
+          <div className="flex-auto p-4">
+            <div
+              className={`ag-theme-alpine ${
+                rowData.length === 0 && isRefreshing ? "opacity-20" : ""
+              }`}
+              style={{ height: "60vh" }}
+            >
+              <AgGridReact
+                ref={gridRef}
+                rowData={rowData}
+                columnDefs={columnDefs}
+                defaultColDef={defaultColDef}
+                animateRows={true}
+                readOnlyEdit={true}
+                enableCellChangeFlash={true}
+                paginationAutoPageSize={true}
+                pagination={true}
+                suppressClickEdit={true}
+                getRowStyle={getRowStyle}
               />
             </div>
-          )}
-          <div className="text-center mt-4">
-            <Tooltip id="action-btn-tooltip" />
-            <button
-              className={`bg-blueGray-500 text-white active:bg-blueGray-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ${
-                isRefreshing ? "opacity-50" : ""
-              }`}
-              type="button"
-              onClick={onRefreshClickHandler}
-              disabled={isRefreshing}
-              data-tooltip-id="action-btn-tooltip"
-              data-tooltip-content="Refresh to view the recently processed examples in the table."
-            >
-              <i
-                className={`fas fa-arrow-rotate-right ${
-                  isRefreshing ? "fa-spin" : ""
+            {isProcessing && (
+              <div className=" mx-auto w-9/12">
+                <ProgressBar
+                  taskInProgress="Processing Abstracts ..."
+                  percentage={percentage}
+                />
+              </div>
+            )}
+            <div className="text-center mt-4">
+              <Tooltip id="action-btn-tooltip" />
+              <button
+                className={`bg-blueGray-500 text-white active:bg-blueGray-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ${
+                  isRefreshing ? "opacity-50" : ""
                 }`}
-              ></i>{" "}
-              {isRefreshing ? "Refreshing..." : "Refresh"}
-            </button>
-            <button
-              className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
-              onClick={onBtnExport}
-              data-tooltip-id="action-btn-tooltip"
-              data-tooltip-content="Click to download the results as a CSV file."
-              data-tooltip-variant="info"
-            >
-              <i className="fas fa-file-export"></i> Export
-            </button>
-            <button
-              className={`bg-red-500 text-white active:bg-red-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150
-              ${isStopping ? "opacity-50" : ""}`}
-              type="button"
-              onClick={onStopClickedHandler}
-              alt="stop model's execution"
-              disabled={isStopping}
-              data-tooltip-id="action-btn-tooltip"
-              data-tooltip-variant="error"
-              data-tooltip-content="Click to halt the processing of examples."
-            >
-              <i className={`fas fa-stop  ${isStopping ? "fa-flip" : ""}`}></i>{" "}
-              {isStopping ? "Stopping..." : "Stop"}
-            </button>
-          </div>
-          {rowData.length === 0 && isRefreshing && (
-            <div
-              role="status"
-              className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
-            >
-              <svg
-                aria-hidden="true"
-                className="w-10 h-10 mr-2 text-gray-200 animate-spin fill-lightBlue-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+                type="button"
+                onClick={onRefreshClickHandler}
+                disabled={isRefreshing}
+                data-tooltip-id="action-btn-tooltip"
+                data-tooltip-content="Refresh to view the recently processed examples in the table."
               >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-              <span className="sr-only">Loading...</span>
+                <i
+                  className={`fas fa-arrow-rotate-right ${
+                    isRefreshing ? "fa-spin" : ""
+                  }`}
+                ></i>{" "}
+                {isRefreshing ? "Refreshing..." : "Refresh"}
+              </button>
+              <button
+                className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={onBtnExport}
+                data-tooltip-id="action-btn-tooltip"
+                data-tooltip-content="Click to download the results as a CSV file."
+                data-tooltip-variant="info"
+              >
+                <i className="fas fa-file-export"></i> Export
+              </button>
+              <button
+                className={`bg-red-500 text-white active:bg-red-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150
+              ${isStopping ? "opacity-50" : ""}`}
+                type="button"
+                onClick={onStopClickedHandler}
+                alt="stop model's execution"
+                disabled={isStopping}
+                data-tooltip-id="action-btn-tooltip"
+                data-tooltip-variant="error"
+                data-tooltip-content="Click to halt the processing of examples."
+              >
+                <i
+                  className={`fas fa-stop  ${isStopping ? "fa-flip" : ""}`}
+                ></i>{" "}
+                {isStopping ? "Stopping..." : "Stop"}
+              </button>
             </div>
-          )}
+            {rowData.length === 0 && isRefreshing && (
+              <div
+                role="status"
+                className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-10 h-10 mr-2 text-gray-200 animate-spin fill-lightBlue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="w-3/12 m-2 mr-0">
-        <CardBarChart
-          data={chartData}
-          options={chartOptions}
-          title="Total Count by Category"
-          subtitle="Analysis Overview"
-        />
-      </div>
+        <div className="w-3/12 m-2 mr-0">
+          <CardBarChart
+            data={chartData}
+            options={chartOptions}
+            title="Total Count by Category"
+            subtitle="Analysis Overview"
+          />
+        </div>
       </div>
     </>
   );
